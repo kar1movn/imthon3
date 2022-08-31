417428eb7a3d4e7783df5778ccc5090414edc0ae
kar1movn <nodir2005karimov.com>
2022-08-31 20:51:44 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//  birimchi misol
+
+
+let arr = ["I", "study", "JavaScript", "right", "now"]
+
+console.log(arr.splice(0));
+
+
+
+// ikkinchi misol
+
+let misol2 = 'salom dunyo';
+
+conlose.log(misol2.split('').reverse());
+
+// uchinhci misol
+function (
+  if( numb % 2 === 0) {
+    return console.log = juft raqam
+  }
+)
